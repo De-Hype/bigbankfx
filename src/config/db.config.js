04bccaeadca8b2a644696918b45e5fed1df985bd
David Hype <iamsocialhype@gmail.com>
2024-05-12 20:16:11 -0700

    Fixed the AppResponse

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 
 const Connect =async ()=>{
     try {
+        console.log(process.env.DB_URI)
         await mongoose.connect(process.env.DB_URI);
         console.log("DB connected succesfully")
     } catch (error) {
+        console.log(error)
         console.error("DB connection failed")
     }
 } 
---
+++ src/helpers/AppResponse.js
@@ -0,0 +1,32 @@
+function AppResponse(res, message, statusCode = 200, data = null) {
+    
+  let status;
+  switch (statusCode) {
+    case (statusCode = 200):
+      status = "ok";
+      break;
+    case (statusCode = 201):
+      status = "created";
+      break;
+    case (statusCode = 202):
+      status = "Accepted";
+      break;
+    case (statusCode = 203):
+      status = "Non-Authoritative Information";
+      break;
+    case (statusCode = 204):
+      status = "No Content";
+      break;
+    case (statusCode = 205):
+      status = "Reset Content";
+      break;
+    case (statusCode = 206):
+      status = "Partial Content";
+      break;
+    default:
+      break;
+  }
+
+  res.status(statusCode).json({ status: status, message: message, data: data });
+}
+module.exports = AppResponse;
