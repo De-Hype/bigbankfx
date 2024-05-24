ec525d171d2b913daae8a71319c101b31759bc6e
David Hype <iamsocialhype@gmail.com>
2024-05-23 18:20:20 -0700

    Started work on some templates

--- src/helpers/CalculateProfits.js
+++ src/helpers/CalculateProfits.js
@@ -1,2 +1,15 @@
-const CalculateDailyProfit = () => {};
+const CalculateDailyProfit = async(plan) => {
+    //For this to work, we are gonna set up a scripts that runs every day
+    //This script will fetch the details of the user and then check their balance
+    //If they have cash in their balance, it will calculate the profit off the amount in the balance.
+    //This should be done for all the users
+    let principal =0;
+    const dailyPercentForBasicPlan=(2/100);
+    const dailyPercentForSilverPlan=(2/100);
+    const dailyPercentForGoldPlan=(2/100);
+    if (plan === "basic"){
+        //Fetch User Details 
+    }
+     
+};
 module.exports = { CalculateDailyProfit };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,64 +1,56 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    publicId:{
-        type:String,
-        required:true,
-        unique:true
+const userSchema = new mongoose.Schema(
+  {
+    publicId: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    first_name:{
-        type:String,
-        required:true
+    first_name: {
+      type: String,
+      required: true,
     },
-    last_name:{
-        type:String,
-        required:true
+    last_name: {
+      type: String,
+      required: true,
     },
-    username:{
-        type:String,
-        required:true
+    username: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,  
+    password: {
+      type: String,
     },
-    plan:{
-        type:String,
-        enum:["basic", "silver", "gold"],
-        required:true,
-        validate:function(value){
-            return ["basic", "silver", "gold"].includes(value);
-        },
-        message:"Invalid plan. Must be either one of basic, silver or gold."
+    plan: {
+      type: String,
+      enum: ["basic", "silver", "gold"],
+      required: true,
+      validate: function (value) {
+        return ["basic", "silver", "gold"].includes(value);
+      },
+      message: "Invalid plan. Must be either one of basic, silver or gold.",
     },
-    total_balance:{
-        type:Number,
+    total_balance: {
+      type: Number,
     },
-    prev_deposit:{
-        type:Number,
+    prev_deposit: {
+      type: Number,
     },
-    new_deposit:{
-        type:Number,
+    new_deposit: {
+      type: Number,
     },
-    profit:{
-        type:Number,
-    }
-    
-    
-    // googleId:{
-    //     type:String,
-    //     default:null
-    // },
-    // githubId:{
-    //     type:String,
-    //     default:null
-    // }
-    
-}, {timestamps:true})
+    profit: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("user", userSchema);
-module.exports = User
+module.exports = User;
